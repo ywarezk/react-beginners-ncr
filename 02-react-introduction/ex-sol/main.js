4734496620c6644815225d64d1ce9d9fa825ee1a
ywarezk <ywarezk@gmail.com>
2021-10-02 19:13:33 +0300

    created the ex solution

---
+++ 02-react-introduction/ex-sol/main.js
@@ -0,0 +1,43 @@
+function Header() {
+    return (
+        <header>
+            <h1>
+                Hello im the header
+            </h1>
+        </header>
+    )
+}
+
+function Login() {
+    return (
+        <form>
+            <input type="email" placeholder="email" name="email" />
+            <input type="password" placeholder="password" name="password" />
+            <button>Login</button>
+        </form>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <h1>
+                Hello im the footer
+            </h1>
+        </footer>
+    )
+}
+
+function LoginPage() {
+    return (
+        <div>
+            <Header />
+
+            <Login />
+
+            <Footer />
+        </div>
+    )
+}
+
+ReactDOM.render(<LoginPage />, document.getElementById('root'));
