cdcaed5100ff2ade81c1bd4b891de649f60d9077
ywarezk <ywarezk@gmail.com>
2021-10-04 08:48:07 +0300

    removed the generated js files

--- 03-typescript/ex-sol/02-modules/modules-b.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var modules_a_1 = require("./modules-a");
-(0, modules_a_1.printObj)({
-    hello: 'world',
-    foo: 'bar'
-});
-//# sourceMappingURL=modules-b.js.map
